1c326b584d1aefa132416ef5d04880e0b0ee06ef
Asymptote <melihokutangame@hotmail.com>
2023-03-13 22:57:25 +0300

    MELİH OKUTAN HASTANESİ YAZDIM

--- HastaneOTO/İletişim.Designer.cs
+++ HastaneOTO/İletişim.Designer.cs
@@ -60,9 +60,9 @@ namespace HastaneOTO
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.Location = new System.Drawing.Point(153, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(296, 33);
+            this.label1.Size = new System.Drawing.Size(445, 33);
             this.label1.TabIndex = 1;
-            this.label1.Text = "NAF HASTANELERİ";
+            this.label1.Text = "MELIH OKUTAN HASTASNESİ";
             // 
             // label2
             // 
@@ -80,7 +80,7 @@ namespace HastaneOTO
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label3.Location = new System.Drawing.Point(0, 159);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.Size = new System.Drawing.Size(56, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "Adres :";
             // 
@@ -90,7 +90,7 @@ namespace HastaneOTO
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label4.Location = new System.Drawing.Point(0, 217);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.Size = new System.Drawing.Size(88, 16);
             this.label4.TabIndex = 4;
             this.label4.Text = "İletişim No :";
             // 
@@ -100,7 +100,7 @@ namespace HastaneOTO
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label5.Location = new System.Drawing.Point(0, 262);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.Size = new System.Drawing.Size(40, 16);
             this.label5.TabIndex = 5;
             this.label5.Text = "Fax :";
             // 
@@ -110,7 +110,7 @@ namespace HastaneOTO
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label6.Location = new System.Drawing.Point(0, 313);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(65, 16);
+            this.label6.Size = new System.Drawing.Size(64, 16);
             this.label6.TabIndex = 6;
             this.label6.Text = "Eposta :";
             // 
@@ -121,7 +121,7 @@ namespace HastaneOTO
             this.label7.ForeColor = System.Drawing.Color.Black;
             this.label7.Location = new System.Drawing.Point(93, 159);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(285, 16);
+            this.label7.Size = new System.Drawing.Size(284, 16);
             this.label7.TabIndex = 7;
             this.label7.Text = "İstanbul/Kartal/Soğanlık/DefneliCaddesi";
             // 
@@ -131,7 +131,7 @@ namespace HastaneOTO
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label8.Location = new System.Drawing.Point(93, 217);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(98, 16);
+            this.label8.Size = new System.Drawing.Size(97, 16);
             this.label8.TabIndex = 8;
             this.label8.Text = "(553)4903960";
             // 
@@ -141,7 +141,7 @@ namespace HastaneOTO
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label9.Location = new System.Drawing.Point(93, 262);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(48, 16);
+            this.label9.Size = new System.Drawing.Size(47, 16);
             this.label9.TabIndex = 9;
             this.label9.Text = "42536";
             // 
@@ -151,7 +151,7 @@ namespace HastaneOTO
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label10.Location = new System.Drawing.Point(93, 313);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(156, 16);
+            this.label10.Size = new System.Drawing.Size(155, 16);
             this.label10.TabIndex = 10;
             this.label10.Text = "anilfb606@gmail.com";
             // 
@@ -162,7 +162,7 @@ namespace HastaneOTO
             this.label11.ForeColor = System.Drawing.Color.Black;
             this.label11.Location = new System.Drawing.Point(93, 185);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(344, 16);
+            this.label11.Size = new System.Drawing.Size(343, 16);
             this.label11.TabIndex = 11;
             this.label11.Text = "/ManolyaSokak/BaharkentSitesi/E-Blok/Daire:10";
             // 
